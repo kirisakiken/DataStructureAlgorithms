928fa94f6f0be57135167b6ae0602c83254f48a9
kirisakiken <bezmican96@gmail.com>
2022-06-18 18:46:25 +0300

    feat(problems/binary-search): leetcode 35 and 852

---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode35.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DataStructureAlgorithms.Problems.LeetCode
+{
+    /// <summary>
+    ///     LeetCode 35 - Search Insert Position
+    ///     https://leetcode.com/problems/search-insert-position/
+    /// </summary>
+    public class LeetCode35
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            int pivot;
+
+            while (left <= right)
+            {
+                pivot = (left + right) / 2;
+                if (nums[pivot] == target)
+                    return pivot;
+
+                if (nums[pivot] > target)
+                    right = pivot - 1;
+
+                if (nums[pivot] < target)
+                    left = pivot + 1;
+            }
+
+            return left;
+        }
+    }
+}
---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode852.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DataStructureAlgorithms.Problems.LeetCode
+{
+    /// <summary>
+    ///     Peak Index in a Mountain Array
+    ///     https://leetcode.com/problems/peak-index-in-a-mountain-array/
+    /// </summary>
+    public class LeetCode852
+    {
+        /// <summary>
+        ///     Linq Solution;
+        ///     var list = arr.ToList();
+        ///     return list.IndexOf(list.Max());
+        /// </summary>
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            var i = 0;
+            while (arr[i] < arr[i + 1])
+                ++i;
+
+            return i;
+        }
+    }
+}
