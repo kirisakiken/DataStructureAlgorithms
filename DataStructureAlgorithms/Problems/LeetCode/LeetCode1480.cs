69a10b716a4ef711efe01014394bd1a5344c437f
kirisakiken <bezmican96@gmail.com>
2022-07-02 17:08:29 +0300

    feat(problems/leetcode75): add leetcode 742 and 1480

---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode1480.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructureAlgorithms.Problems.LeetCode
+{
+    /// <summary>
+    ///     LeetCode 1480 - Running Sum of 1d Array
+    ///     https://leetcode.com/problems/running-sum-of-1d-array/
+    /// </summary>
+    public class LeetCode1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var res = new int[nums.Length];
+            res[0] = nums[0];
+            for (var i = 1; i <= nums.Length - 1; ++i)
+                res[i] = res[i - 1] + nums[i];
+
+            return res;
+        }
+    }
+}
---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode724.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace DataStructureAlgorithms.Problems.LeetCode
+{
+    /// <summary>
+    ///     LeetCode 742 - Find Pivot Index
+    ///     https://leetcode.com/problems/find-pivot-index/
+    ///     O(n) - time
+    ///     O(1) - space
+    /// </summary>
+    public class LeetCode724
+    {
+        public int PivotIndex(int[] nums)
+        {
+            var sum = nums.Sum();
+            var leftSum = 0;
+
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                // nums[i] = PIVOT
+                if (leftSum == sum - leftSum - nums[i])
+                    return i;
+
+                leftSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
