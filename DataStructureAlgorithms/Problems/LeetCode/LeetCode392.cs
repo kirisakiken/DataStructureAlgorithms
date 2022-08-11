04a174d0842902b9639edaff2ea28b409be4fd81
kirisakiken <bezmican96@gmail.com>
2022-08-11 19:31:59 +0300

    feat(problems/leetcode75): add leetcode 392

---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode392.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructureAlgorithms.Problems.LeetCode
+{
+    /// <summary>
+    ///     LeetCode 392 - Is Subsequence
+    ///     https://leetcode.com/problems/is-subsequence/
+    /// </summary>
+    public class LeetCode392
+    {
+        
+    }
+
+    // C++ Solution
+    //
+    // bool isSubsequence(std::string s, std::string t)
+    // {
+    //     int j = 0;
+    //     for (int i = 0; i < t.length() && j < s.length(); ++i)
+    //     {
+    //         if (s[j] == t[i])
+    //             ++j;
+    //     }
+    //
+    //     return (j == s.length());
+    // }
+}
