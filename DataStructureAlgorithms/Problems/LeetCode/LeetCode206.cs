3db9f330a01df289ab73825cc4d45416166b877e
kirisakiken <bezmican96@gmail.com>
2022-09-28 20:52:53 +0300

    feat(problems/leetcode206) add reverse linked list problem

---
+++ DataStructureAlgorithms/Problems/LeetCode/LeetCode206.cs
@@ -0,0 +1,38 @@
+﻿namespace DataStructureAlgorithms.Problems.LeetCode
+{
+     public class ListNode
+     {
+         public int Value { get; }
+         public ListNode Next { get; set; }
+
+         public ListNode(int val, ListNode next)
+         {
+             Value = val;
+             Next = next;
+         }
+     }
+
+    /// <summary>
+    ///     LeetCode 206 - Reverse Linked List
+    ///     https://leetcode.com/problems/reverse-linked-list/
+    /// </summary>
+    public class LeetCode206
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode previous = null;
+            ListNode current = head;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+
+            return previous;
+        }
+    }
+}
